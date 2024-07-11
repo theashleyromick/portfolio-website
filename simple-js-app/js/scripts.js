321067bc5f96152e1044d811d554cf3a34989cdb
theashleyromick <ashley.romick@gmail.com>
2024-07-11 11:46:51 -0400

    erase nonrelevant data/ create pokemonList
    I have erased my old data from Exercise 1.1. I then added my variable 'pokemonList', with several objects to the array.

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world')
-
-let favoriteFood = 'Pizza';
-document.write (favoriteFood);
+let pokemonList = [
+    { name: 'Squirtle', height: '0.5 m', type: 'Water'},
+    { name: 'Charmander', height: '0.6 m', type: 'Fire'},
+    { name: 'Pikachu', height: '0.4 m', type: 'Electric'}
+];
