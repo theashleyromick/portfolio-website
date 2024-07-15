726dc6a0a81301d62c4e65161f6d2a9402a6c383
theashleyromick <ashley.romick@gmail.com>
2024-07-15 11:21:37 -0400

    add loop with condition
    I added a loop into my code with the condition that any Pokemon over 0.6 m would say "Wow, That's big!"

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,5 +1,21 @@
 let pokemonList = [
     { name: 'Squirtle', height: '0.5 m', type: 'Water'},
     { name: 'Charmander', height: '0.6 m', type: 'Fire'},
-    { name: 'Pikachu', height: '0.4 m', type: 'Electric'}
+    { name: 'Pikachu', height: '0.7 m', type: 'Electric'}
 ];
+
+//Flag to ensure only one Pokemon gets the "Wow, That;s big!" label
+let bigPokemonFlag = false;
+
+for (let i = 0; i < pokemonList.length; i++) {
+    let type = pokemonList[i].type;
+    let name = pokemonList[i].name;
+    let height = pokemonList[i].height;
+      if (parseFloat (height) > 0.6 && ! bigPokemonFlag) {
+        document.write(name + ': ' + height + ' - ' +type + '- Wow, that\'s big!<br>')
+        bigPokemonFlag = true;
+      } else {
+        document.write (name + ': ' + height + ' - ' + type + '<br>');
+      }
+    }
+
