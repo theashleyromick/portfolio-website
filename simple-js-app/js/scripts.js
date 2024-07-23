365e467b7de45f6436f22d4f4e157401bc3f2e36
theashleyromick <ashley.romick@gmail.com>
2024-07-23 10:10:04 -0400

    fix broken code. remove 'm' after height.

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,12 +1,18 @@
 let pokemonList = [
-    { name: 'Squirtle', height: '0.5 m', type: 'Water'},
-    { name: 'Charmander', height: '0.6 m', type: 'Fire'},
-    { name: 'Pikachu', height: '0.7 m', type: 'Electric'}
+    { name: 'Squirtle', height: '0.5', type: 'Water'},
+    { name: 'Charmander', height: '0.6', type: 'Fire'},
+    { name: 'Pikachu', height: '0.7', type: 'Electric'}
 ];
 
-pokemonList.forEach(function(pokemon) {
-    console.log(pokemon.name + ' is ') + pokemon.height + ' meters tall ' + pokemon.type + ' and is this type pg Pokemon!'
-});
-
-//Flag to ensure only one Pokemon gets the "Wow, That;s big!" label
+// Flag to ensure only one Pokemon gets the "Wow, that's big!" label
 let bigPokemonFlag = false;
+
+pokemonList.forEach(function(pokemon) {
+    console.log(pokemon.name + ' is ' + pokemon.height + ' meters tall and is a ' + pokemon.type + ' type Pokemon!');
+    if (parseFloat(pokemon.height) > 0.6 && !bigPokemonFlag) {
+        document.write(pokemon.name + ': ' + pokemon.height + ' - ' + pokemon.type + ' - Wow, that\'s big!<br>');
+        bigPokemonFlag = true;
+    } else {
+        document.write(pokemon.name + ': ' + pokemon.height + ' - ' + pokemon.type + '<br>');
+    }
+});
