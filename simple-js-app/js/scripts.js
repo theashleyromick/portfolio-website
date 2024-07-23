0802a1db1ecb7ea8b1aec548e620d00df17d2392
theashleyromick <ashley.romick@gmail.com>
2024-07-23 09:57:47 -0400

    change for loop for forEach loop

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -4,18 +4,9 @@ let pokemonList = [
     { name: 'Pikachu', height: '0.7 m', type: 'Electric'}
 ];
 
+pokemonList.forEach(function(pokemon) {
+    console.log(pokemon.name + ' is ') + pokemon.height + ' meters tall ' + pokemon.type + ' and is this type pg Pokemon!'
+});
+
 //Flag to ensure only one Pokemon gets the "Wow, That;s big!" label
 let bigPokemonFlag = false;
-
-for (let i = 0; i < pokemonList.length; i++) {
-    let type = pokemonList[i].type;
-    let name = pokemonList[i].name;
-    let height = pokemonList[i].height;
-      if (parseFloat (height) > 0.6 && ! bigPokemonFlag) {
-        document.write(name + ': ' + height + ' - ' +type + '- Wow, that\'s big!<br>')
-        bigPokemonFlag = true;
-      } else {
-        document.write (name + ': ' + height + ' - ' + type + '<br>');
-      }
-    }
-
