5756f7de482433b1bba3ee41dc623f68e80ea967
theashleyromick <ashley.romick@gmail.com>
2024-07-25 12:44:59 -0400

    change button background color

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,48 +1,48 @@
-let pokemonRepository = (function() {
-    let pokemonList = [
-        { name: 'Squirtle', height: '0.5', type: 'Water'},
-        { name: 'Charmander', height: '0.6', type: 'Fire'},
-        { name: 'Pikachu', height: '0.7', type: 'Electric'}
-    ];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Squirtle", height: "0.5", type: "Water" },
+    { name: "Charmander", height: "0.6", type: "Fire" },
+    { name: "Pikachu", height: "0.7", type: "Electric" },
+  ];
 
-    function getAll () {
-      return pokemonList;
-    }
+  function getAll() {
+    return pokemonList;
+  }
 
-    function add(pokemon) {
-      pokemonList.push(pokemon);
-    }
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-    function addListItem(pokemon) {
-  let pokemonListElement = document.querySelector('.pokemon-list');
+  function addListItem(pokemon) {
+    let pokemonListElement = document.querySelector(".pokemon-list");
 
-  let listItem = document.createElement('li');
+    let listItem = document.createElement("li");
 
-  let button = document.createElement('button');
-  button.innerText = pokemon.name;
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
 
-  button.classList.add('pokemon-button');
+    button.classList.add("pokemon-button");
 
-  button.addEventListener('click', function() {
-    showDetails(pokemon);
-  });
+    button.addEventListener("click", function () {
+      showDetails(pokemon);
+    });
 
-  listItem.appendChild(button);
+    listItem.appendChild(button);
 
-  pokemonListElement.appendChild(listItem);
+    pokemonListElement.appendChild(listItem);
   }
 
   function showDetails(pokemon) {
     console.log(pokemon);
   }
 
-    return {
-      getAll: getAll,
-      add: add,
-      addListItem: addListItem
-    };
+  return {
+    getAll: getAll,
+    add: add,
+    addListItem: addListItem,
+  };
 })();
 
-pokemonRepository.getAll().forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function (pokemon) {
   pokemonRepository.addListItem(pokemon);
 });
